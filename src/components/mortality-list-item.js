313f6900afe396158796cb5f7def2c74142cef1d
Franck-Pro <franck.fernandez@gmail.com>
2018-09-07 15:26:37 +0200

    Add Graph

--- src/components/mortality-list-item.js
+++ src/components/mortality-list-item.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import Flag from './flag'
+import ReactChartkick, { ColumnChart} from 'react-chartkick'
+import Chart from 'chart.js'
+
+const xTitle = "Age"
+const yTitle = "% Mortalité"
+
+ReactChartkick.addAdapter(Chart)
+window.Chart = require('chart.js');
     
     const MortalityListItem = () => {
         return (
             <div>
                 <Flag className="flag-medium" country="France"/>
+                <ColumnChart xtitle={xTitle} ytitle={yTitle} data={[["Sun", 32], ["Mon", 46], ["Tue", 28], ["Mo", 4], ["o", 42]]} />
             </div>
         )
     }
